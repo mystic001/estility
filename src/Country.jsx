2ecb352ad8a2716902999136be907c6fb3b393a8
Bamidele Emmanuel <bamideleomonayin@gmail.com>
2023-12-19 09:57:34 -0600

    Add: Ui changes

---
+++ src/Country.jsx
@@ -0,0 +1,46 @@
+import React, {useState} from 'react'
+
+function Country() {
+
+    const [country, setCountry] = useState({})
+    const [name, setName]= useState('')
+    
+    const handleSubmit = async (name)=>{
+        console.log('helo')
+        console.log("country name",name)
+        const url = `https://restcountries.com/v3.1/name/${name}?fullText=true`
+        fetch(url).then(res=> res.json()).then(data=> setCountry(data[0]))
+    }
+
+    const handleinput = (e)=>{
+            setName(e.target.value)
+        }
+
+console.log(country)
+  return (
+    <>
+   
+    <div>Country</div>
+    <div>
+    <div>
+        <img alt = '' src = {country?.flags?.png}/>
+        <h4> {country?.name?.official}</h4>
+        
+        <h4>{country?.continents !== undefined ? country?.continents[0] : ''}</h4>
+        <h6>{country?.population !== undefined ? country?.population : ''}</h6>
+        <h6>{country?.languages.eng}</h6>
+        <h6>{country?.currencies}</h6>
+    </div>
+    <div>2</div>
+    </div>
+
+    <div>
+        <input placeholder='Country name' onChange={handleinput}/>
+        <button onClick = {()=>handleSubmit(name)}>Get details</button>
+    </div>
+    </>
+    
+  )
+}
+
+export default Country
